0bfc7c92be95b276ee72ba90db7ac55ab8587e09
tmorris <tmorris@student.42.fr>
2020-04-08 00:50:02 +0200

    Finish JS linting

--- notemaker/static/notemaker/js/displayCard.js
+++ notemaker/static/notemaker/js/displayCard.js
@@ -1,131 +1,137 @@
+/* globals $, document, console, window, confirm */
+/* jshint unused: false, regexpu: false */
 function FlashCard(card) {
-    this.card_type = card.card_type;
-    this.id = card.id;
-    this.due_date = card.due_date;
-    this.word = card.note.word;
-    this.ipa = card.note.ipa;
-    this.grammar = card.note.grammar;
-    this.definition = card.note.definition;
-    this.example = card.note.example;
-    this.expression = card.note.expression;
-    this.expression_meaning = card.note.expression_meaning;
-    this.image = card.note.image;
-    this.note_id = card.note.id;
+	this.card_type = card.card_type;
+	this.id = card.id;
+	this.due_date = card.due_date;
+	this.word = card.note.word;
+	this.ipa = card.note.ipa;
+	this.grammar = card.note.grammar;
+	this.definition = card.note.definition;
+	this.example = card.note.example;
+	this.expression = card.note.expression;
+	this.expression_meaning = card.note.expression_meaning;
+	this.image = card.note.image;
+	this.note_id = card.note.id;
 
-    this.getHTML = function() {
-        var functionName = "draw"+this.card_type;
-        return this[functionName]();
-    };
-    
-    this.drawBack = function(){
-        return "<div class=\'back\'><b>"+ this.word +"</b> /"+ this.ipa +"/ <a class=\'mp3\' msg=\'"+ this.word.replace(/[']/ig, "") +"\' href=\'#\'>&#128265;</a><br>Definition: "+ this.definition +"<br>Example: "+ this.example +"<br><img src=\'data:image/jpg;base64, "+ this.image +"\'><br></div><div class=\'rate_bar_rate\' style=\'display: none;\'><button id=\'rate_0\' class=\'rate_button\' card_id=\'"+this.id+"\'>Incorrect</button><button id=\'rate_1\' class=\'rate_button\' card_id=\'"+this.id+"\'>Correct</button><button id=\'rate_2\' class=\'rate_button\' card_id=\'"+this.id+"\'>Easy</button><button class=\'rate_button\' id=\'rate_9\'>Skip Card</button><button class='delete_button' card_id=\'"+this.id+"\'>Delete</button><button class='edit_button' note_id=\'"+this.note_id+"\'>Edit</button></div>";
-    };
-    
-    this.drawAnswerButton = function(){
-        return "<div class='rate_bar_show'><button class='flipCard'>Show Answer</button><button class='rate_button' id='rate_9'>Skip Card</button><button class='delete_button' card_id=\'"+this.id+"\'>Delete</button></div>";
-    }
-    
-    this.drawImageToWord = function(){
-        let result = "<span class='flashcard'><div class='front'>";
-        result += "What word goes with this image:<br><img src='data:image/jpg;base64, "+ this.image +"'></div>";
-        
-        result += this.drawAnswerButton();
-        result += this.drawBack();
-        result += "</span>";
-        return result;
-    };
-    
-    this.drawWordToImage = function(){
-        let result = "<span class='flashcard'><div class='front'>";
-        result += "What does this word mean:<br><b>"+ this.word +"</b> /"+ this.ipa +"/ <a class='mp3' msg='"+ this.word.replace(/[']/ig, "") +"' href='#'>&#128265;</a></div>"
-        
-        result += this.drawAnswerButton();        
-        result += this.drawBack();        
-        result += "</span>";
-        return result;
-    };
-    
-    this.drawFillInTheBlank = function(){
-        let blankedExample = this.example.replace(this.word, "____");
-        let result = "<span class='flashcard'><div class='front'>";
-        result += "Fill in the Blank:<br>"+ blankedExample +"</div>";
-        
-        result += this.drawAnswerButton();
-        
-        let boldedExample = this.example.replace(this.word, "<b>"+this.word+"</b>");
-        result += "<div class='back'>"+ boldedExample +"<a class='mp3' msg='"+ this.example.replace(/[']/ig, "") +"' href='#'>&#128265;</a><br><br><b>"+ this.word +"</b> /"+ this.ipa +"/ <a class='mp3' msg='"+ this.word.replace(/[']/ig, "") +"' href='#'>&#128265;</a><br>Definition: "+ this.definition +"<br><img src='data:image/jpg;base64, "+ this.image +"'><br></div><div class='rate_bar_rate' style='display: none;'><button id='rate_0' class='rate_button' card_id='"+this.id+"'>Incorrect</button><button id='rate_1' class='rate_button' card_id='"+this.id+"'>Correct</button><button id='rate_2' class='rate_button' card_id='"+this.id+"'>Easy</button><button class='rate_button' id='rate_9'>Skip Card</button></div><button class='delete_button' card_id=\'"+this.id+"\'>Delete</button><button class='edit_button' card_id=\'"+this.id+"\'>Edit</button>";
-        
-        result += "</span>";
-        return result;
-    };
-};
+	this.getHTML = function () {
+		let functionName = "draw" + this.card_type;
+		return this[functionName]();
+	};
 
-function flipCard(){
-    console.log("flipping card");
-    $("#resultWindow").find(".front").toggle();
-    $("#resultWindow").find(".back").toggle();
-    $("#resultWindow").find(".rate_bar_show").toggle();
-    $("#resultWindow").find(".rate_bar_rate").toggle();
-};
+	this.drawBack = function () {
+		return "<div class=\'back\'><b>" + this.word + "</b> /" + this.ipa + "/ <a class=\'mp3\' msg=\'" + this.word.replace(/[']/ig, "") + "\' href=\'#\'>&#128265;</a><br>Definition: " + this.definition + "<br>Example: " + this.example + "<br><img src=\'data:image/jpg;base64, " + this.image + "\'><br></div><div class=\'rate_bar_rate\' style=\'display: none;\'><button id=\'rate_0\' class=\'rate_button\' card_id=\'" + this.id + "\'>Incorrect</button><button id=\'rate_1\' class=\'rate_button\' card_id=\'" + this.id + "\'>Correct</button><button id=\'rate_2\' class=\'rate_button\' card_id=\'" + this.id + "\'>Easy</button><button class=\'rate_button\' id=\'rate_9\'>Skip Card</button><button class='delete_button' card_id=\'" + this.id + "\'>Delete</button><button class='edit_button' note_id=\'" + this.note_id + "\'>Edit</button></div>";
+	};
+	this.drawAnswerButton = function () {
+		return "<div class='rate_bar_show'><button class='flipCard'>Show Answer</button><button class='rate_button' id='rate_9'>Skip Card</button><button class='delete_button' card_id=\'" + this.id + "\'>Delete</button></div>";
+	};
 
-function showFrontOfCard(){
-    $("#resultWindow").find(".front").show();
-    $("#resultWindow").find(".back").hide();
-    $("#resultWindow").find(".rate_bar_show").show();
-    $("#resultWindow").find(".rate_bar_rate").hide();
+	this.drawImageToWord = function () {
+		let result = "<span class='flashcard'><div class='front'>";
+		result += "What word goes with this image:<br><img src='data:image/jpg;base64, " + this.image + "'></div>";
+
+		result += this.drawAnswerButton();
+		result += this.drawBack();
+		result += "</span>";
+		return result;
+	};
+
+	this.drawWordToImage = function () {
+		let result = "<span class='flashcard'><div class='front'>";
+		result += "What does this word mean:<br><b>" + this.word + "</b> /" + this.ipa + "/ <a class='mp3' msg='" + this.word.replace(/[']/ig, "") + "' href='#'>&#128265;</a></div>";
+
+		result += this.drawAnswerButton();
+		result += this.drawBack();
+		result += "</span>";
+		return result;
+	};
+
+	this.drawFillInTheBlank = function () {
+		let blankedExample = this.example.replace(this.word, "____");
+		let result = "<span class='flashcard'><div class='front'>";
+		result += "Fill in the Blank:<br>" + blankedExample + "</div>";
+
+		result += this.drawAnswerButton();
+
+		let boldedExample = this.example.replace(this.word, "<b>" + this.word + "</b>");
+		result += "<div class='back'>" + boldedExample + "<a class='mp3' msg='" + this.example.replace(/[']/ig, "") + "' href='#'>&#128265;</a><br><br><b>" + this.word + "</b> /" + this.ipa + "/ <a class='mp3' msg='" + this.word.replace(/[']/ig, "") + "' href='#'>&#128265;</a><br>Definition: " + this.definition + "<br><img src='data:image/jpg;base64, " + this.image + "'><br></div><div class='rate_bar_rate' style='display: none;'><button id='rate_0' class='rate_button' card_id='" + this.id + "'>Incorrect</button><button id='rate_1' class='rate_button' card_id='" + this.id + "'>Correct</button><button id='rate_2' class='rate_button' card_id='" + this.id + "'>Easy</button><button class='rate_button' id='rate_9'>Skip Card</button></div><button class='delete_button' card_id=\'" + this.id + "\'>Delete</button><button class='edit_button' card_id=\'" + this.id + "\'>Edit</button>";
+
+		result += "</span>";
+		return result;
+	};
 }
 
-$( document ).on('click', '.flipCard', function(){
-    flipCard();
-    $("#resultWindow").trigger('isFlipped');
+function flipCard() {
+	console.log("flipping card");
+	$("#resultWindow").find(".front").toggle();
+	$("#resultWindow").find(".back").toggle();
+	$("#resultWindow").find(".rate_bar_show").toggle();
+	$("#resultWindow").find(".rate_bar_rate").toggle();
+}
+
+function showFrontOfCard() {
+	$("#resultWindow").find(".front").show();
+	$("#resultWindow").find(".back").hide();
+	$("#resultWindow").find(".rate_bar_show").show();
+	$("#resultWindow").find(".rate_bar_rate").hide();
+}
+
+$(document).on('click', '.flipCard', function () {
+	flipCard();
+	$("#resultWindow").trigger('isFlipped');
 });
 
-$( document ).on('click', '.rate_button', function(){
-    let value = $(this).attr('id')[5];
-    //console.log(value);
-    if (value == '9'){
-        console.log("This would be a 9 string");
-        showFrontOfCard();
-        $( "#resultModal" ).trigger( "finish-card", [value] );
-    } else{
-        $.ajax({
-            url: "../ajax/rate_card/",
-            data: {'card_id': $(this).attr('card_id'), 'rating': value},
-            dataType: 'json',
-            success: function (data) {
-                //console.log(data);
-                $( "#resultModal" ).trigger( "finish-card", [value] );
-            }
-        });
-    }
+$(document).on('click', '.rate_button', function () {
+	let value = $(this).attr('id')[5];
+	//console.log(value);
+	if (value === '9') {
+		console.log("This would be a 9 string");
+		showFrontOfCard();
+		$("#resultModal").trigger("finish-card", [value]);
+	} else {
+		$.ajax({
+			url: "../ajax/rate_card/",
+			data: {
+				'card_id': $(this).attr('card_id'),
+				'rating': value
+			},
+			dataType: 'json',
+			success: function (data) {
+				//console.log(data);
+				$("#resultModal").trigger("finish-card", [value]);
+			}
+		});
+	}
 });
 
-$( document ).on('click', '.delete_button', function(){
-    let r = confirm("Are you sure you want to delete this card? (This action CANNOT be undone)");
-    if (r == true) {
-        $.ajax({
-            url: "../ajax/delete_card/",
-            data: {'card_id': $(this).attr('card_id')},
-            dataType: 'json',
-            success: function (data) {
-                $( "#resultModal" ).trigger( "finish-card", [0] );
-            }
-        });    
-    }
+$(document).on('click', '.delete_button', function () {
+	let r = confirm("Are you sure you want to delete this card? (This action CANNOT be undone)");
+	if (r === true) {
+		$.ajax({
+			url: "../ajax/delete_card/",
+			data: {
+				'card_id': $(this).attr('card_id')
+			},
+			dataType: 'json',
+			success: function (data) {
+				$("#resultModal").trigger("finish-card", [0]);
+			}
+		});
+	}
 });
 
-$( document ).on('click', '.edit_button', function(){
-  /*  let r = confirm("Are you sure you want to delete this card? (This action CANNOT be undone)");
-    if (r == true) {
-        $.ajax({
-            url: "../ajax/delete_card/",
-            data: {'card_id': $(this).attr('card_id')},
-            dataType: 'json',
-            success: function (data) {
-                $( "#resultModal" ).trigger( "finish-card", [0] );
-            }
-        });    
-    } */
-  //$( document ).load( "http://127.0.0.1:8000/notemaker/note/"+ $( this ).attr('note_id') + "/update/");
-  window.location.href = "http://127.0.0.1:8000/notemaker/note/"+ $( this ).attr('note_id') + "/update/"
+$(document).on('click', '.edit_button', function () {
+	/*  let r = confirm("Are you sure you want to delete this card? (This action CANNOT be undone)");
+	if (r === true) {
+	$.ajax({
+	url: "../ajax/delete_card/",
+	data: {'card_id': $(this).attr('card_id')},
+	dataType: 'json',
+	success: function (data) {
+	$( "#resultModal" ).trigger( "finish-card", [0] );
+	}
+	});
+	} */
+	//$( document ).load( "http://127.0.0.1:8000/notemaker/note/"+ $( this ).attr('note_id') + "/update/");
+	window.location.href = "http://127.0.0.1:8000/notemaker/note/" + $(this).attr('note_id') + "/update/";
 });
--- notemaker/static/notemaker/js/tts.js
+++ notemaker/static/notemaker/js/tts.js
@@ -1,16 +1,17 @@
-read_msg = function (msg, lang){
-    console.log('Playing ' + msg + ' using browser TTS');
-    let utterance = new SpeechSynthesisUtterance(msg);
-    utterance.lang = lang;
-    window.speechSynthesis.speak(utterance);
-}
+/* globals console, document, window, SpeechSynthesisUtterance, $ */
+let read_msg = function (msg, lang) {
+	console.log('Playing ' + msg + ' using browser TTS');
+	let utterance = new SpeechSynthesisUtterance(msg);
+	utterance.lang = lang;
+	window.speechSynthesis.speak(utterance);
+};
 
-$( document ).on('click', '.mp3', function(){
-    let msg_text = $(this).attr("msg");
-    read_msg(msg_text, "fr-FR");
+$(document).on('click', '.mp3', function () {
+	let msg_text = $(this).attr("msg");
+	read_msg(msg_text, "fr-FR");
 });
 
-$("#resultWindow").bind('isFlipped', function(){
-    let msg_text = $("#resultWindow").find(".back").find(".mp3").attr("msg");
-    read_msg(msg_text, "fr-FR");
+$("#resultWindow").bind('isFlipped', function () {
+	let msg_text = $("#resultWindow").find(".back").find(".mp3").attr("msg");
+	read_msg(msg_text, "fr-FR");
 });
