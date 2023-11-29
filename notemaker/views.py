2a826fda1603ab4f761b4299addd363b445543e1
tmorris <zq8fznrj@duck.com>
2023-11-29 18:49:38 +0100

    feat: require login on remaining views

--- notemaker/views.py
+++ notemaker/views.py
@@ -25,31 +25,32 @@ from .models import Note, SearchResult, Card, CardType
 class IndexView(TemplateView):
     template_name = "notemaker/index.html"
     
-class NoteDetailView(DetailView):
+class NoteDetailView(LoginRequiredMixin, DetailView):
     model = Note
     
-class NoteUpdateView(generic.edit.UpdateView):
+class NoteUpdateView(LoginRequiredMixin, generic.edit.UpdateView):
     model = Note
     fields = '__all__'
     template_name_suffix = '_update_form'
     success_url = reverse_lazy('notemaker:htmx-review-card')
 
-class NoteCreateView(generic.edit.CreateView):
+class NoteCreateView(LoginRequiredMixin, generic.edit.CreateView):
     model = Note
     fields = '__all__'
     
-class NoteListView(ListView):
+class NoteListView(LoginRequiredMixin, ListView):
     model = Note
     
-class CardListView(ListView):
+class CardListView(LoginRequiredMixin, ListView):
     model = Card
 
 class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = "notemaker/dashboard.html"
     
-class WorkshopView(TemplateView):
+class WorkshopView(LoginRequiredMixin, TemplateView):
     template_name = "notemaker/workshop.html"
 
+@login_required
 def create_note_form(request):
     if request.method == "POST":
         form = NoteForm(request.POST)
@@ -59,6 +60,7 @@ def create_note_form(request):
         form = NoteForm({'word': 'testing'})
     return render(request, "notemaker/note_form.html", {"form": form})
 
+@login_required
 def test_ajax(request):
     rec = request.GET.get('rec', None)
     data = {
@@ -124,6 +126,7 @@ def ajax_anki_create_note(request):
     
     return JsonResponse(data)
 
+@login_required
 def ajax_anki_generate_note(request):
     word = request.GET.get('word', None)
     
@@ -143,6 +146,7 @@ def ajax_anki_generate_note(request):
     return JsonResponse(data)
     # return render(request, "notemaker/note_create.html", context)
 
+@login_required
 def htmx_generate_note(request):
     logger = logging.getLogger("view:htmx_generate_note")
     logger.setLevel(logging.DEBUG)
@@ -184,16 +188,19 @@ def htmx_generate_note(request):
             return render(request, "notemaker/utils/message.html", { "message": 'Error: could not generate note card' })
     return render(request, "notemaker/note_form.html", {"form": form, "data": data})
 
+@login_required
 def ajax_note_detail_view(request):
     note_id = request.GET.get('note_id', None)
     context = {'note': Note.objects.get(pk=note_id)}
     return render(request, "notemaker/note_detail.html", context)
 
+@login_required
 def ajax_note_update_view(request):
     note_id = request.GET.get('note_id', None)
     context = {'note': Note.objects.get(pk=note_id)}
     return render(request, "notemaker/note_detail.html", context)
 
+@login_required
 def ajax_rate_card_view(request):
     card_id = request.GET.get('card_id', None)
     card = Card.objects.get(pk=card_id)
@@ -211,6 +218,7 @@ def ajax_rate_card_view(request):
     data = {'success': 'true', 'new_date': new_date}
     return JsonResponse(data)
 
+@login_required
 def htmx_rate_card_view(request, pk, rating):
     card = Card.objects.get(pk=pk)
     if rating == '0':
@@ -228,12 +236,14 @@ def htmx_rate_card_view(request, pk, rating):
     card.schedule()
     return htmx_review_card(request)
 
+@login_required
 def htmx_skip_card_view(request, pk):
     card = Card.objects.get(pk=pk)
     card.due_date = timezone.now()
     card.save()
     return htmx_review_card(request)
 
+@login_required
 def htmx_review_card(request):
     max_number = 1
     cards = Card.custom_objects.are_due(request.user)[:max_number]
@@ -243,6 +253,7 @@ def htmx_review_card(request):
         return render(request, "notemaker/card_detail.html", context)
     return render(request, "notemaker/utils/message.html", { "message": "No cards to review" })
 
+@login_required
 def ajax_delete_card_view(request):
     card_id = request.GET.get('card_id', None)
     card = Card.objects.get(pk=card_id)
@@ -253,6 +264,7 @@ def ajax_delete_card_view(request):
         data = {'success': 'false', 'result': f'Card {card_id} could not be deleted due to an unknown error.'}
     return JsonResponse(data)
 
+@login_required
 def card_detail_view(request, pk):
     card_obj = Card.objects.get(pk=pk)
     card_type_obj = CardType.objects.get(pk=card_obj.card_type.pk)
@@ -262,10 +274,12 @@ def card_detail_view(request, pk):
                "notemaker/card_detail.html",
                context)
 
+@login_required
 def ajax_card_detail_view(request):
     card_id = request.GET.get('card_id', None)
     return card_detail_view(request, card_id)
 
+@login_required
 def ajax_review_cards(request):
     #data = request.GET.get('data', None)
     #max_number = int(data['max_number'])
