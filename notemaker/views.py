2d3697f961803bfa436122e0471620d0ae0c14d3
tmorris <zq8fznrj@duck.com>
2023-11-29 23:21:47 +0100

    fix: protect views
    - ajax_delete_card_view
    - htmx_generate_note
    
    Re #24
    Re #25

--- notemaker/views.py
+++ notemaker/views.py
@@ -1,13 +1,7 @@
 import logging
-import json
-import base64
-from random import shuffle
-import requests
 
 from notemaker.utils.get_search_result import get_search_result, set_defaults
 from .forms import NoteForm
-
-from thscraper.thscraper import query_all
 
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
@@ -15,73 +9,39 @@ from django.views import generic
 from django.views.generic.base import TemplateView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse_lazy
 from django.utils import timezone
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 
-from .models import Note, SearchResult, Card, CardType
+from .models import Note, Card, CardType
 
 class IndexView(TemplateView):
     template_name = "notemaker/index.html"
-    
+
+## Protect This
 class NoteDetailView(LoginRequiredMixin, DetailView):
     model = Note
-    
+
+## Protect This
 class NoteUpdateView(LoginRequiredMixin, generic.edit.UpdateView):
     model = Note
     fields = '__all__'
     template_name_suffix = '_update_form'
     success_url = reverse_lazy('notemaker:htmx-review-card')
-
-class NoteCreateView(LoginRequiredMixin, generic.edit.CreateView):
-    model = Note
-    fields = '__all__'
     
+## Only list User's Notes
 class NoteListView(LoginRequiredMixin, ListView):
     model = Note
-    
+
+## Only list User's Cards
 class CardListView(LoginRequiredMixin, ListView):
     model = Card
 
 class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = "notemaker/dashboard.html"
     
-@login_required
-def create_note_form(request):
-    if request.method == "POST":
-        form = NoteForm(request.POST)
-        if form.is_valid():
-            return HttpResponse("Form submitted!")
-    else:
-        form = NoteForm({'word': 'testing'})
-    return render(request, "notemaker/note_form.html", {"form": form})
-
-@login_required
-def test_ajax(request):
-    rec = request.GET.get('rec', None)
-    data = {
-        'result': f"You sent us: {rec}, and now we're sending it back to you."
-    }
-    return JsonResponse(data)
-
-def deserialize(serial):
-    results = dict()
-    if len(serial) == 0:
-        return results
-    try:
-        items = serial.split('&')
-    except ValueError:
-        items = [serial]
-    for item in items:
-        key, value = item.split("=")
-        #if key in results:
-        #    results[key].append(value)
-        #else:
-        #    results[key] = [value]
-        results[key] = value
-    return results
-    
+ 
 @login_required
 def htmx_generate_note(request):
     logger = logging.getLogger("view:htmx_generate_note")
@@ -91,6 +51,7 @@ def htmx_generate_note(request):
         logger.info(form.errors)
         if form.is_valid():
             if form.cleaned_data['owner'] != request.user:
+                return render(request, "notemaker/utils/message.html", { "message": 'Invalid Owner' })
             new_note = form.save(commit=True)
             i2w = CardType.objects.get(card_type_name="ImageToWord")
             Card(note=new_note, card_type=i2w).save()
