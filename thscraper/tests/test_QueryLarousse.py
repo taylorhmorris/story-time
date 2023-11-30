03e8e4f355f758c5651c03d98bfd563ef4c58065
tmorris <zq8fznrj@duck.com>
2023-11-30 21:57:18 +0100

    fix(thscraper): Larousse expressions parsing

--- thscraper/tests/test_QueryLarousse.py
+++ thscraper/tests/test_QueryLarousse.py
@@ -4,7 +4,7 @@ from ..queries.QueryLarousse import QueryLarousse
 
 class Test(unittest.TestCase):
     def test_query_larousse(self):
-        results = QueryLarousse(False).query('chat')
+        results = QueryLarousse(True).query('chat')
         self.assertEqual(len(results["definitions"]), 8)
         for definition in results['definitions']:
             self.assertNotRegexpMatches(definition['definition'], '^[1-9]')
